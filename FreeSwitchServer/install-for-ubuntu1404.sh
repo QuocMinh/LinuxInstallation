f1b0d81eed3bf439b3cdc8419f6bdbdba8d2be07
QuocMinh <cqminh1995@gmail.com>
2017-04-13 10:36:41 +0700

    Update install-for-ubuntu1404.sh

--- FreeSwitchServer/install-for-ubuntu1404.sh
+++ FreeSwitchServer/install-for-ubuntu1404.sh
@@ -1,6 +1,5 @@
 wget -O - https://files.freeswitch.org/repo/ubuntu/freeswitch-unstable/freeswitch_archive_g0.pub | apt-key add -;
 sudo echo "deb http://files.freeswitch.org/repo/ubuntu/freeswitch-unstable/ trusty main" > /etc/apt/sources.list.d/freeswitch.list;
-
 # you may want to populate /etc/freeswitch at this point.
 # if /etc/freeswitch does not exist, the standard vanilla configuration is deployed
 sudo mkdir /etc/freeswitch;
