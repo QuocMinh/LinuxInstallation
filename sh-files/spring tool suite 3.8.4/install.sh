fbe0337945c172fa8ff5dc91658b31ec0eb8a5d7
Chau Quoc Minh <cqminh1995@gmail.com>
2017-06-04 13:30:01 +0700

    Install Spring tool suite 3.8.4

---
+++ sh-files/spring tool suite 3.8.4/install.sh
@@ -0,0 +1,5 @@
+cd /opt/;
+sudo tar -xzvf '/media/cqm/DATA/Developer'\''s_Tools/Eclipse/spring-tool-suite-3.8.4.RELEASE-e4.6.3-linux-gtk-x86_64.tar.gz';
+
+sudo gedit /usr/share/applications/sts.desktop;
+gedit '/media/cqm/SOFTWARE/Programs_LINUX/LinuxInstallation/sh-files/spring tool suite 3.8.4/sts-desktop.txt';
